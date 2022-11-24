a156f0c184c870bd099cc8c148a123881906b740
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-11-24 20:17:48 +0545

    added axis

--- components/BarGraph.js
+++ components/BarGraph.js
@@ -1,6 +1,7 @@
 import React, { useMemo } from "react";
 import { Bar } from "@visx/shape";
 import { Group } from "@visx/group";
+import { AxisLeft, AxisBottom } from "@visx/axis";
 import * as d3 from "d3";
 import { scaleBand, scaleLinear } from "@visx/scale";
 
@@ -11,15 +12,26 @@ const BarGraph = ({ data, width, height }) => {
   const xMax = width;
   const yMax = height - verticalMargin;
 
-  const xScale = d3
-    .scaleBand()
-    .range([0, xMax])
-    .domain(barData.map((el) => el.letter))
-    .padding(0.4);
+  const xScale = useMemo(
+    () =>
+      d3
+        .scaleBand()
+        .range([0, xMax])
+        .domain(barData.map((el) => el.letter))
+        .padding(0.4),
+    [xMax, barData]
+  );
 
-  const yScale = scaleLinear()
-    .range([yMax, 0])
-    .domain([0, Math.max(...barData.map((el) => Number(el.frequency) * 100))]);
+  const yScale = useMemo(
+    () =>
+      scaleLinear()
+        .range([yMax, 0])
+        .domain([
+          0,
+          Math.max(...barData.map((el) => Number(el.frequency) * 100)),
+        ]),
+    [yMax, barData]
+  );
 
   return (
     <svg width={width} height={height}>
@@ -29,7 +41,7 @@ const BarGraph = ({ data, width, height }) => {
         fill={d3.color("steelblue")}
         rx={14}
       />
-      <Group top={verticalMargin / 2}>
+      <Group top={verticalMargin / 2} left={30}>
         {barData.map((el, i) => {
           const letter = el.letter;
           const barWidth = xScale.bandwidth();
@@ -47,6 +59,28 @@ const BarGraph = ({ data, width, height }) => {
             />
           );
         })}
+        <AxisBottom
+          numTicks={barData.length}
+          top={yMax}
+          scale={xScale}
+          tickLabelProps={() => ({
+            fill: d3.color("#f3f3f3"),
+            fontSize: 11,
+            textAnchor: "middle",
+          })}
+        />
+        <AxisLeft
+          scale={yScale.nice()}
+          numTicks={10}
+          top={0}
+          tickLabelProps={(e) => ({
+            fill: d3.color("#f3f3f3"),
+            fontSize: 10,
+            textAnchor: "end",
+            x: -12,
+            y: (yScale(e) ?? 0) + 3,
+          })}
+        />
       </Group>
     </svg>
   );
