ac1ae78f4acc497e5b4b5ea1a5644e9a25e897ed
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-11-24 18:45:52 +0545

    fix graph

--- components/BarGraph.js
+++ components/BarGraph.js
@@ -1,45 +1,50 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Bar } from "@visx/shape";
 import { Group } from "@visx/group";
 import * as d3 from "d3";
+import { scaleBand, scaleLinear } from "@visx/scale";
 
 const BarGraph = ({ data, width, height }) => {
-  const barData = data.slice(0, 5);
-  console.log(barData);
-  const yMargin = 140;
+  const barData = data.slice(5);
+  const verticalMargin = 120;
 
   const xMax = width;
-  const yMax = height - yMargin;
+  const yMax = height - verticalMargin;
 
   const xScale = d3
     .scaleBand()
-    .domain(barData.map((el) => el.letters))
     .range([0, xMax])
-    .padding(0.3);
+    .domain(barData.map((el) => el.letter))
+    .padding(0.4);
 
-  const yScale = d3
-    .scaleLinear()
+  const yScale = scaleLinear()
     .range([yMax, 0])
-    .domain([0, d3.max([...barData.map((el) => Number(el.frequency) * 100)])]);
+    .domain([0, Math.max(...barData.map((el) => Number(el.frequency) * 100))]);
 
   return (
-    <svg height={height} width={width}>
-      <Group top={yMargin / 2}>
+    <svg width={width} height={height}>
+      <rect
+        width={width}
+        height={height}
+        fill={d3.color("steelblue")}
+        rx={14}
+      />
+      <Group top={verticalMargin / 2}>
         {barData.map((el, i) => {
           const letter = el.letter;
           const barWidth = xScale.bandwidth();
           const barHeight = yMax - yScale(Number(el.frequency) * 100);
-          console.log(yScale(Number(el.frequency) * 100));
           const barX = xScale(letter);
-          const barY = barHeight;
+          const barY = yMax - barHeight;
           return (
-            <Bar key={i}>
+            <Bar
+              key={i}
               x={barX}
               y={barY}
               width={barWidth}
               height={barHeight}
-              fill={"rgba(23, 233, 217, .5)"}
-            </Bar>
+              fill={d3.color("#f7fbff")}
+            />
           );
         })}
       </Group>
