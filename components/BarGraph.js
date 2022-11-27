831bdbb24d0bbf6e601f133692433fc849aa1b4e
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-11-27 15:48:03 +0545

    refactored

--- components/BarGraph.js
+++ components/BarGraph.js
@@ -2,52 +2,44 @@ import React, { useMemo } from "react";
 import { Bar } from "@visx/shape";
 import { Group } from "@visx/group";
 import { AxisLeft, AxisBottom } from "@visx/axis";
-import * as d3 from "d3";
-import { scaleBand, scaleLinear } from "@visx/scale";
 
-const BarGraph = ({ data, width, height }) => {
-  const barData = data.slice(5);
+const BarGraph = ({
+  barData,
+  axisLeft,
+  axisBottom,
+  width,
+  height,
+  buildXScale,
+  buildYScale,
+  xItem,
+  yItem,
+  colors,
+}) => {
   const verticalMargin = 120;
   const marginLeft = 30;
+  const { leftScale, leftNumTicks, leftTop, leftTickLabelProps } = axisLeft;
+  const { bottomNumTicks, bottomTickLabelProps } = axisBottom;
 
   const xMax = width - marginLeft;
   const yMax = height - verticalMargin;
 
-  const xScale = useMemo(
-    () =>
-      d3
-        .scaleBand()
-        .range([0, xMax])
-        .domain(barData.map((el) => el.letter))
-        .padding(0.3),
-    [xMax, barData]
-  );
-
-  const yScale = useMemo(
-    () =>
-      scaleLinear()
-        .range([yMax, 0])
-        .domain([
-          0,
-          Math.max(...barData.map((el) => Number(el.frequency) * 100)),
-        ]),
-    [yMax, barData]
-  );
+  const xScale = buildXScale(xMax);
+  const yScale = buildYScale(yMax);
 
   return (
     <svg width={width} height={height}>
       <rect
         width={width}
         height={height}
-        fill={d3.color("steelblue")}
+        fill={colors.backgroundColor}
         rx={14}
       />
       <Group top={verticalMargin / 2} left={marginLeft}>
         {barData.map((el, i) => {
-          const letter = el.letter;
+          const item = xItem(el);
           const barWidth = xScale.bandwidth();
-          const barHeight = yMax - yScale(Number(el.frequency) * 100);
-          const barX = xScale(letter);
+          const barHeight = yMax - yScale(yItem(el));
+          const barX = xScale(item);
           const barY = yMax - barHeight;
           return (
             <Bar
@@ -56,31 +48,21 @@ const BarGraph = ({ data, width, height }) => {
               y={barY}
               width={barWidth}
               height={barHeight}
-              fill={d3.color("#f7fbff")}
+              fill={colors.barColor}
             />
           );
         })}
         <AxisBottom
-          numTicks={barData.length}
+          numTicks={bottomNumTicks}
           top={yMax}
           scale={xScale}
-          tickLabelProps={() => ({
-            fill: d3.color("#f3f3f3"),
-            fontSize: 11,
-            textAnchor: "middle",
-          })}
+          tickLabelProps={() => bottomTickLabelProps}
         />
         <AxisLeft
-          scale={yScale.nice()}
-          numTicks={10}
-          top={0}
-          tickLabelProps={(e) => ({
-            fill: d3.color("#f3f3f3"),
-            fontSize: 10,
-            textAnchor: "end",
-            x: -12,
-            y: (yScale(e) ?? 0) + 3,
-          })}
+          scale={leftScale(yScale)}
+          numTicks={leftNumTicks}
+          top={leftTop}
+          tickLabelProps={(e) => leftTickLabelProps(yScale(e))}
         />
       </Group>
     </svg>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,75 @@
-import React from "react";
+import React, { useMemo } from "react";
 import BarGraph from "../components/BarGraph";
 import LetterFrequency from "@visx/mock-data/lib/mocks/letterFrequency";
 import { ParentSize } from "@visx/responsive";
 import style from "../styles/Home.module.css";
+import { scaleBand, scaleLinear } from "@visx/scale";
+import * as d3 from "d3";
 
 const index = () => {
+  const data = LetterFrequency.slice(5);
+
+  const buildXScale = (xMax) => {
+    return d3
+      .scaleBand()
+      .range([0, xMax])
+      .domain(data.map((el) => el.letter))
+      .padding(0.3);
+  };
+
+  const buildYScale = (yMax) => {
+    return scaleLinear()
+      .range([yMax, 0])
+      .domain([0, Math.max(...data.map((el) => Number(el.frequency) * 100))]);
+  };
+
+  const axisLeft = {
+    leftScale: (yScale) => yScale.nice(),
+    leftNumTicks: 10,
+    leftTop: 0,
+    leftTickLabelProps: (yScale) => {
+      return {
+        fill: "#f3f3f3",
+        fontSize: 10,
+        textAnchor: "end",
+        x: -12,
+        y: yScale + 3,
+      };
+    },
+  };
+
+  const axisBottom = {
+    bottomNumTicks: data.length,
+    bottomTickLabelProps: {
+      fill: "yellow",
+      fontSize: 11,
+      textAnchor: "middle",
+    },
+  };
+
+  const xItem = (el) => el.letter;
+  const yItem = (el) => Number(el.frequency) * 100;
+  const colors = {
+    backgroundColor: d3.color("steelblue"),
+    barColor: "white",
+  };
+
   return (
     <div className={style.chart}>
       <ParentSize>
-        {({ width, height }) => (
-          <BarGraph data={LetterFrequency} width={width} height={height} />
+        {({ width }) => (
+          <BarGraph
+            barData={data}
+            axisLeft={axisLeft}
+            axisBottom={axisBottom}
+            width={width}
+            height={700}
+            buildXScale={buildXScale}
+            buildYScale={buildYScale}
+            xItem={xItem}
+            yItem={yItem}
+            colors={colors}
+          />
         )}
       </ParentSize>
     </div>
